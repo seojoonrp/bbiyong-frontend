15423afd4910c077c827824267518e299964c127
seojoonrp <seojoonrp@snu.ac.kr>
2026-01-01 16:56:57 +0900

    feat: Download social login libraries

---
+++ declarations.d.ts
@@ -0,0 +1,11 @@
+// declarations.d.ts
+
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module "*.woff2";
+declare module "*.woff";
